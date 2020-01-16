75beaa6943764edd3e309a4806952663ca174417
Daffy <mystery.of.duck@gmail.com>
2020-01-16 15:32:25 +0100

    no message

--- bytecode.py
+++ bytecode.py
@@ -1,21 +1,33 @@
 bytecode = {
-    "IADD":1,
-    "ISUB":2,
-    "IMUL":3,
-    "ILT":4,
-    "IEQ":5,
-    "BR":6,
-    "BRT":7,
-    "BRF":8,
-    "ICONST":9,
-    "LOAD":10,
-    "GLOAD":11,
-    "STORE":12,
-    "GSTORE":13,
-    "PRINT":14,
-    "POP":15,
-    "HALT":16
-
+    "IADD":{"opcode":1,"narg":0},
+    "ISUB":{"opcode":2,"narg":0},
+    "IMUL":{"opcode":3,"narg":0},
+    "ILT":{"opcode":4,"narg":0},
+    "IEQ":{"opcode":5,"narg":0},
+    "BR":{"opcode":6,"narg":1},
+    "BRT":{"opcode":7,"narg":1},
+    "BRF":{"opcode":8,"narg":1},
+    "ICONST":{"opcode":9,"narg":1},
+    "LOAD":{"opcode":10,"narg":1},
+    "GLOAD":{"opcode":11,"narg":1},
+    "STORE":{"opcode":12,"narg":1},
+    "GSTORE":{"opcode":13,"narg":1},
+    "PRINT":{"opcode":14,"narg":0},
+    "POP":{"opcode":15,"narg":0},
+    "HALT":{"opcode":16,"narg":0}
 }
+
 def instruction(str):
-    return bytecode[str]
+    return bytecode[str]["opcode"]
+
+def opcode(i):
+    for key,value in bytecode.items():
+        if value["opcode"] == i:
+            return (key,value["narg"])
+
+def is_True(i):
+    return i > 0
+
+
+def is_False(i):
+    return i == 0
--- vm.py
+++ vm.py
@@ -1,38 +1,138 @@
-from bytecode import instruction
+from bytecode import instruction, is_False, is_True
+from bytecode import opcode as from_opcode
 class VM ():
     def __init__(self, code, main, datasize):
-        self.data = [None] * datasize
+        self.globals = [None] * datasize
         self.code = code
         self.stack = [None] * 1000
         self.ip = main
         self.sp = -1
         self.fp = -1
-    
+        self.debug = True
+        self.trace = "--------------------------------------\n"
+        self.h = -1
+    def _globals_dumps(self):
+        self.trace += "--------------------------------------\nGlobals:\n--------------------------------------\n"
+        adr_format = '{:>03}'
+        for i in range(len(self.globals)):
+            if self.globals[i] != None:
+                address = adr_format.format(i)
+                value = str(self.globals[i])
+                self.trace += address + ":\t" + value + "\n"
+
+
+    def _add_trace(self, opcode):
+        adr_format = '{:>03}'
+        address = adr_format.format(str(self.ip - from_opcode(opcode)[1] - 1))
+        trace_opcode = from_opcode(opcode)[0]
+        args = str(self.code[self.ip-1])
+        stack = str(self.stack[:self.sp + 1])
+        if(from_opcode(opcode)[1] == 0):
+            self.trace = self.trace +address + " : "+ trace_opcode + "\t\t" +  stack + "\n"
+        if(from_opcode(opcode)[1] == 1):
+            self.trace = self.trace +address + " : "+ trace_opcode + "\t"+ args + "\t" + stack + "\n"
+
     def cpu(self):
-        while self.ip < len(self.code):
+        while self.ip < len(self.code) and self.h != 0:
             opcode = self.code[self.ip]
             self.ip += 1
             if opcode == instruction("ICONST"):
                 value = self.code[self.ip]
-                self.ip +=1
-                self.sp +=1
+                self.ip += 1
+                self.sp += 1
                 self.stack[self.sp] = value
+            if opcode == instruction("IADD"):
+                value2 = self.stack[self.sp]
+                self.sp -= 1
+                value1 = self.stack[self.sp]
+                self.sp -= 1
+                result = value1 + value2
+                self.sp += 1
+                self.stack[self.sp] = result
+            if opcode == instruction("ISUB"):
+                value2 = self.stack[self.sp]
+                self.sp -= 1
+                value1 = self.stack[self.sp]
+                self.sp -= 1
+                result = value1 - value2
+                self.sp += 1
+                self.stack[self.sp] = result
+            if opcode == instruction("IMUL"):
+                value2 = self.stack[self.sp]
+                self.sp -= 1
+                value1 = self.stack[self.sp]
+                self.sp -= 1
+                result = value1 * value2
+                self.sp += 1
+                self.stack[self.sp] = result
+            if opcode == instruction("ILT"):
+                value2 = self.stack[self.sp]
+                self.sp -= 1
+                value1 = self.stack[self.sp]
+                self.sp -= 1
+                result = int(value1 < value2)
+                self.sp += 1
+                self.stack[self.sp] = result
+            if opcode == instruction("IEQ"):
+                value2 = self.stack[self.sp]
+                self.sp -= 1
+                value1 = self.stack[self.sp]
+                self.sp -= 1
+                result = int(value1 == value2)
+                self.sp += 1
+                self.stack[self.sp] = result
+            if opcode == instruction("BR"):
+                self.ip = self.code[self.ip]
+            if opcode == instruction("BRT"):
+                addr = self.code[self.ip]
+                value = self.stack[self.sp]
+                self.sp -= 1
+                if(is_True(value)):
+                    self.ip = addr
+            if opcode == instruction("BRF"):
+                addr = self.code[self.ip]
+                value = self.stack[self.sp]
+                self.sp -= 1
+                if(is_False(value)):
+                    self.ip = addr
+            if opcode == instruction("GSTORE"):
+                value = self.stack[self.sp]
+                self.sp -= 1
+                addr = self.code[self.ip]
+                self.ip += 1
+                self.globals[addr] = value
+            if opcode == instruction("GLOAD"):
+                addr = self.code[self.ip]
+                self.ip += 1
+                value = self.globals[addr]
+                self.sp += 1
+                self.stack[self.sp] = value
+            if opcode == instruction("POP"):
+                self.sp -= 1
             if opcode == instruction("PRINT"):
                 value = self.stack[self.sp]
                 self.sp -= 1
                 print(value)
             if opcode == instruction("HALT"):
-                return
+                self.h = 0
+            if(self.debug):
+                self._add_trace(opcode)
+        if(self.debug):
+            self._globals_dumps()
+        return self.h
 
 
 def main():
     code = [
-    instruction("ICONST"),99,
-    instruction("PRINT"),
-    instruction("HALT")
+    instruction("ICONST"),11,
+    instruction("ICONST"),10,
+    instruction("ILT"),
+    instruction("GSTORE"),0,
     ]
-    vm = VM(code,0,0)
+    vm = VM(code,0,10)
     vm.cpu()
+    if vm.debug == True:
+        print(vm.trace)
     
 
 if __name__ == "__main__":
